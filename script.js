21c13bb1b2cd80d0ecbb539f1f9f5dc434ac931b
PratikDamodar <117561545+PratikDamodar@users.noreply.github.com>
2023-04-18 15:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,10 +13,3 @@ function hasKey(key) {
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
-const key = prompt("Enter Key.");
-alert(hasKey(key));
-}
-
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
