8dd5fa48b9890f590f27472b15de8302d1290c8f
PratikDamodar <117561545+PratikDamodar@users.noreply.github.com>
2023-04-18 15:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
-function hasKey(key) {
-  //   write your code here
 	for (let i in sampleObject){
 		if(i == key){
 			return true
@@ -15,6 +11,8 @@ function hasKey(key) {
 }
 
 // Do not change the code below
+const key = prompt("Enter Key.");
+alert(hasKey(key));
 const key = prompt("Enter Key.");
 alert(hasKey(key));
 }
